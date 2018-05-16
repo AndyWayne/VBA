67ba3ea160e9ad3be60a1de69d1bfd3b16217e6c
Andy Wayne <Andy.wayne@foxmail.com>
2018-05-16 10:45:37 +0300

    Update renameBatch.sh

--- renameBatch.sh
+++ renameBatch.sh
@@ -6,13 +6,14 @@
 
 TARDIR="new"
 
+#Function for change the name of the files in batch.
 function renameBatch(){
   j=1;
   for i in `find ./ -type f -name "info_*"`; do
     if [ $j -le 9 ]; then
       cp $i ./${TARDIR}/info-000$j.log;
     elif [ $j -le 99 ]; then
-       cp $i ./${TARDIR}/info-00$j.log;
+      cp $i ./${TARDIR}/info-00$j.log;
     elif [ $j -le 999 ]; then
       cp $i ./${TARDIR}/info-0$j.log;
     elif [ $j -le 9999 ]; then
