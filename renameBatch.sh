3bd504f7ca9169b3539c4225890cd73f98f5494a
Andy Wayne <Andy.wayne@foxmail.com>
2018-05-16 10:43:08 +0300

    Update renameBatch.sh

--- renameBatch.sh
+++ renameBatch.sh
@@ -4,16 +4,30 @@
 #E-mail: andy.wayne@foxmail.com
 #Function: Rename the files in batch
 
-j=1;
-for i in `find ./ -type f -name "info_*"`; do
-  if [ $j -le 9 ]; then
-    cp $i /home/temp/info-000$j.log;
-  elif [ $j -le 99 ]; then
-    cp $i /home/temp/info-00$j.log;
-  elif [ $j -le 999 ]; then
-    cp $i /home/temp/info-0$j.log;
-  elif [ $j -le 9999 ]; then
-    cp $i /home/temp/info-$j.log;
-  fi
-  ((j++));
-done
+TARDIR="new"
+
+function renameBatch(){
+  j=1;
+  for i in `find ./ -type f -name "info_*"`; do
+    if [ $j -le 9 ]; then
+      cp $i ./${TARDIR}/info-000$j.log;
+    elif [ $j -le 99 ]; then
+       cp $i ./${TARDIR}/info-00$j.log;
+    elif [ $j -le 999 ]; then
+      cp $i ./${TARDIR}/info-0$j.log;
+    elif [ $j -le 9999 ]; then
+      cp $i ./${TARDIR}/info-$j.log;
+    fi
+    ((j++));
+  done
+}
+
+if [ -d ${TARDIR} ];then
+  echo "The ${TARDIR} directory is exist."
+  exit 0;
+else
+  mkdir $TARDIR;
+  renameBatch;
+  echo "All the files are moved to ${TARDIR}."
+fi
+
